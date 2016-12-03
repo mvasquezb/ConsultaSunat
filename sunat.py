21f62d24ce6dd1104be95f54a5e6d05318603226
Martín Vásquez <pedromvasquezb@gmail.com>
2016-12-02 19:56:36 -0500

    Data extraction is done. TODO: accept RUC argument

--- sunat.py
+++ sunat.py
@@ -7,16 +7,35 @@ import requests
 import bs4
 import sys
 import re
-from collections import namedtuple
+import collections
+import logging
+import logging.config
+from datetime import datetime
+import json
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('sunat')
 
 """
 Tipo CIIU que define una actividad economica de un contribyente
 """
-CIIU = namedtuple('CIIU', ['codigo', 'desc', 'rev'])
+CIIU = collections.namedtuple('CIIU', ['cod', 'desc', 'rev'])
 
-DeudaCoactiva = namedtuple('DeudaCoactiva', ['monto', 'periodo_tributario', 'fecha_inicio', 'entidad_asociada'])
+DeudaCoactiva = collections.namedtuple('DeudaCoactiva', ['monto', 'periodo_tributario', 'fecha_inicio', 'entidad_asociada'])
 
-url_sunat = 'http://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias'
+url_base = 'http://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/'
+url_consulta = url_base + 'jcrS00Alias'
+
+class SunatJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if not isinstance(obj, datetime):
+            return json.dumps(obj, ensure_ascii=False, indent=2)
+        return [
+            obj.year,
+            obj.month,
+            obj.day
+        ]
+
 
 @contextlib.contextmanager
 def browse(driver):
@@ -38,8 +57,8 @@ def get_subimage(source, loc, size):
 def get_text_from_image(image):
     tools = pyocr.get_available_tools()
     if len(tools) == 0:
-        print("No OCR tool found")
-        exit(1)
+        logger.error("No OCR tool found")
+        raise ValueError("No OCR tool found")
 
     tool = tools[0]
 
@@ -48,12 +67,9 @@ def get_text_from_image(image):
 def get_captcha_image(driver, frame_elem):
     img_xpath = '//img[@src="captcha?accion=image"]'
     driver.switch_to.frame(frame_elem)
-    try:
-        driver.save_screenshot('image.png')
-        img_elem = driver.find_element_by_xpath(img_xpath)
-    except NoSuchElementException as e:
-        print(e)
-        return ''
+
+    driver.save_screenshot('image.png')
+    img_elem = driver.find_element_by_xpath(img_xpath)
 
     loc = img_elem.location
     size = img_elem.size
@@ -65,8 +81,6 @@ def get_captcha_image(driver, frame_elem):
 
 def get_captcha_text(driver, frame_elem):
     captcha = get_captcha_image(driver, frame_elem)
-    if not captcha:
-        return ''
     return get_text_from_image(captcha)
 
 def save_results(driver, filename):
@@ -143,7 +157,17 @@ def get_ciiu_in_comments(soup):
                 selectEnd = True
 
     ciiu = [ clean_ciiu_str(ci) for ci in ciiu ]
+    ciiu = [ CIIU(ci[0], ci[1], 3) for ci in ciiu ]
+
     return ciiu
+
+def get_ciiu_revision(ciiu_list):
+    revision_3 = [ ci.cod for ci in ciiu_list if type(ci) is CIIU ]
+    to_delete = []
+    for index, ci in enumerate(ciiu_list):
+        if type(ci) is not CIIU and ci[0] not in revision_3:
+            ciiu_list[index] = CIIU(ci[0], ci[1], 4)
+
 
 def get_ciiu_contribuyente(soup):
     ciiu = []
@@ -155,56 +179,127 @@ def get_ciiu_contribuyente(soup):
     options = [ clean_ciiu_str(op.get_text()) for op in options ]
     ciiu += options
 
+    get_ciiu_revision(ciiu)
+    ciiu = list(filter(lambda x: type(x) is CIIU, ciiu))
     return ciiu
 
-def get_deuda_from_row(row):
-    cells = [cell.get_text().strip() for cell in row.find_all('td')]
-    
-    monto = float(cells[0])
-    periodo_tributario = cells[1]
-    fecha_inicio = cells[2]
-    entidad_asociada = cells[3]
+def get_extended_info_attr(params, accion, func_from_row):
+    if not isinstance(params, collections.Mapping):
+        raise TypeError("params is not dictionary")
+    if type(accion) is not str:
+        raise TypeError("accion is not string")
+    if not callable(func_from_row):
+        raise TypeError("func_from_row is not callable")
 
-    return DeudaCoactiva(monto, periodo_tributario, fecha_inicio, entidad_asociada)
-
-def get_deuda_coactiva_contribuyente(params):
-    params['accion'] = 'getInfoDC'
-    res = requests.get(url_sunat, params)
+    params['accion'] = accion
+    try:
+        res = requests.get(url_consulta, params, timeout=5)
+    except requests.exceptions.Timeout:
+        raise TimeoutException("Couldn't connect to {accion} within {time} seconds".format(accion, 5))
     soup = bs4.BeautifulSoup(res.text, 'lxml')
 
     # First table for the title, second for the results of the query
     tables = soup.find_all('table')
     results_table = tables[1]
-    print(results_table)
     intro_cell = results_table.find('td', {'class': 'bgn'})
-
-    deudas = []
+    
+    attr_list = []
     if intro_cell.get_text().strip().startswith('No'):
-        # There are no debts
-        return deudas
+        # There are no records
+        return attr_list
 
-    # There are debts
+    # There are records
     debt_table = results_table.find('table').find('table')
     # Discard header row
     rows = debt_table.find_all('tr')[1:]
+    
+    # Check if table only has error message (case with 'Actas Probatorias')
+    if rows[0].find('td').get_text().strip().startswith('No'):
+        return attr_list
+
+    # Everything is fine, continue parsing
     for row in rows:
-        deudas.append(get_deuda_from_row(row))
+        attr_list.append(func_from_row(row))
+
+    return attr_list
+
+def get_deuda_from_row(row):
+    values = [ cell.get_text().strip() for cell in row.find_all('td') ]
+    
+    if len(values) != 4:
+        raise ValueError("Incorrect number of attributes for '{name}' record".format(name='Deuda Coactiva'))
+
+    monto = float(values[0])
+    periodo_tributario = values[1]
+    periodo_tributario = tuple([ int(val) for val in periodo_tributario.split('-') ])
+    fecha = datetime.strptime(values[2], "%d/%m/%Y")
+    entidad_asociada = values[3]
+
+    return DeudaCoactiva(monto, periodo_tributario, fecha, entidad_asociada)
+
+def get_ot_from_row(row):
+    values = [ cell.get_text().strip() for cell in row.find_all('td') ]
+
+    if len(values) != 2:
+        raise ValueError("Incorrect number of attributes for '{name}' record".format(name='Omision Tributaria'))
+
+    periodo_tributario = values[0]
+    periodo_tributario = tuple([ int(val) for val in periodo_tributario.split('-') ])
+    tributo = values[1]
+
+    return (periodo_tributario, tributo)
+
+def get_acta_prob_from_row(row):
+    values = [ cell.get_text().strip() for cell in row.find_all('td') ]
+
+    if len(values) != 2:
+        raise ValueError("Incorrect number of attributes for '{name}' record".format(name='Acta Probatoria'))
+
+    num_acta = int(values[0])
+    fecha = datetime.strptime(values[1], '%d/%m/%Y')
+    lugar = values[2]
+    infraccion = values[3]
+    desc_infraccion = values[4]
+    ri_roz = values[5]
+    acta_recon = values[6]
+
+    return (num_acta, fecha, lugar, infraccion, desc_infraccion, ri_roz, acta_recon)
+
+def get_deuda_coactiva_contribuyente(params):
+    deudas = get_extended_info_attr(params, 'getInfoDC', get_deuda_from_row)
     return deudas
 
-def add_hidden_data(data):
+def get_omision_tributaria_contribuyente(params):
+    ot = get_extended_info_attr(params, 'getInfoOT', get_ot_from_row)
+    return ot
+"""
+# NOT WORKING: Requires too much customization for now
+def get_acta_probatoria_contribuyente(params):
+    actas = get_extended_info_attr(params, 'getActPro', get_acta_prob_from_row)
+    return actas
+"""
+def add_extended_info(data):
     """
     Get data hidden through buttons
     """
+    if data is None:
+        return None
+
     params = {
         'nroRuc': data['ruc'],
         'desRuc': data['nombre'],
     }
     data['deuda_coactiva'] = get_deuda_coactiva_contribuyente(params)
+    data['omision_tributaria'] = get_omision_tributaria_contribuyente(params)
 
 def parse_results_file(filename):
     with open(filename, 'r') as f:
         text = f.read()
     html = bs4.BeautifulSoup(text, "lxml")
+
+    error = html.find('p', {'class': 'error'})
+    if error is not None:
+        raise AttributeError(error.get_text())
 
     data = {}
 
@@ -223,8 +318,7 @@ def get_data_by_ruc(driver, search_frame, ruc, captcha):
         captcha_input = driver.find_element_by_xpath('//input[@name="codigo"]')
         submit_btn = driver.find_element_by_xpath('//input[@value="Buscar"]')
     except NoSuchElementException as e:
-        print(e)
-        return None
+        raise NoSuchElementException(eval(e.msg)['errorMessage'])
 
     ruc_input.send_keys(str(ruc))
     captcha_input.send_keys(str(captcha))
@@ -238,37 +332,62 @@ def get_data_by_ruc(driver, search_frame, ruc, captcha):
 
     data = parse_results_file('frame.xml')
 
-    add_hidden_data(data)
+    add_extended_info(data)
 
     return data
 
 def main():
     search_frame_xpath = '//frame[@src="frameCriterioBusqueda.jsp"]'
 
+    # User defined
+    #ruc = '20331066703'
+    #ruc = '20141528069'
+    ruc = '20159253539'
+    #ruc = '20217932565'
+    max_retries = 5
+    filename = 'resultado.txt'
+
+    retry = True
+    num_retries = 0
     with browse(webdriver.PhantomJS()) as driver:
-        driver.get(url_sunat)
-        try:
-            search_frame = driver.find_element_by_xpath(search_frame_xpath)
-        except NoSuchElementException as e:
-            print(e)
-            return
+        driver.set_page_load_timeout(5)
+        while retry and num_retries < max_retries:
+            num_retries += 1
+            try:
+                driver.get(url_consulta)
+            except TimeoutException:
+                logger.error("Page load timed out")
+                logger.info('Waiting before retry...')
+                driver.implicitly_wait(5)
+                continue
 
-        captcha = get_captcha_text(driver, search_frame)
-        print("Text in captcha:", captcha)
-        if not captcha or len(captcha) != 4:
-            print("Error reading captcha")
-            return
+            try:
+                search_frame = driver.find_element_by_xpath(search_frame_xpath)
+            except NoSuchElementException as e:
+                logger.error("Incorrect xpath for search frame: %s", search_frame_xpath)
+                continue
+
+            captcha = get_captcha_text(driver, search_frame)
+            logger.info("Text in captcha: %s", captcha)
+            if not captcha or len(captcha) != 4:
+                logger.error("Error reading captcha: %s", captcha)
+                continue
+            
+            data = None
+            try:
+                data = get_data_by_ruc(driver, search_frame, ruc, captcha)
+            except Exception as e:
+                logger.error(e)
+                continue
+
+            with open(filename, 'w') as f:
+                json.dump(data, f, ensure_ascii=False, indent=2, cls=SunatJSONEncoder)
+            retry = False
+            logger.info("Request finished successfully. Results saved to: %s", filename)
         
-        #ruc = '20331066703'
-        #ruc = '20141528069'
-        ruc = '20159253539'
-        data = None
-        try:
-            data = get_data_by_ruc(driver, search_frame, ruc, captcha)
-        except NoSuchElementException as e:
-            print(e)
+        if num_retries >= max_retries:
+            logger.info("Max number of retries reached. Exiting...")
 
-        print(data)
 
 if __name__ == '__main__':
     main()
